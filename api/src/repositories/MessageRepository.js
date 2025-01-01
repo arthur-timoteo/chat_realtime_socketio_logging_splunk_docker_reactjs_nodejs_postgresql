dd00db9edfe6d4a5a6d2c9055861a17019026d7f
arthur-timoteo <arthur.timoteo@gmail.com>
2024-12-31 21:06:48 -0300

    fix: Adjust API to work according to the frontend project

---
+++ api/src/repositories/MessageRepository.js
@@ -0,0 +1,30 @@
+const database = require('../database/database_connection');
+
+class MessageRepository {
+    async create(fk_conversation, fk_member, content_text) {
+
+        await database.query(
+            'INSERT INTO message (fk_conversation, fk_member, content_text) VALUES ($1, $2, $3);', 
+            [fk_conversation, fk_member, content_text]
+        );
+    }
+
+    async list(pk_conversation) {
+
+        const result = await database.query(
+            `SELECT
+                mes.pk,
+                mes.fk_member,
+                mes.content_text,
+                mes.sent_at
+            FROM message AS mes 
+            WHERE mes.fk_conversation = $1
+            ORDER BY mes.sent_at DESC`, 
+            [pk_conversation]
+        );
+        
+        return result.rows;
+    }
+}
+  
+module.exports = new MessageRepository();
