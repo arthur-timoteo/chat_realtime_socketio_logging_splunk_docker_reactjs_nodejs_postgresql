b5d868f72cfd63ea5c56b97d8b3a79501d39a32b
arthur <arthur.timoteo@gmail.com>
2025-06-02 16:58:09 -0300

    feat: [FRONTEND] Implement create a group

---
+++ frontend/src/components/GroupModal/index.tsx
@@ -0,0 +1,114 @@
+import './style.css';
+import { useEffect, useState } from 'react';
+import { api } from '../../services/axios';
+
+interface GroupModalProps {
+    pkMember: string,
+    createGroup: (pk_conversation: string) => void 
+}
+
+interface Contact {
+    pk: string,
+    first_name: string
+}
+
+export function GroupModal({ pkMember, createGroup }: GroupModalProps) {
+    const [contacts, setContacts] = useState<Contact[]>([]);
+    const [titleGroup, setTitleGroup] = useState<string>('');
+    const [selectedContacts, setSelectedContacts] = useState<Contact[]>([]);
+    
+    useEffect(() =>{
+        searchContacts();
+    },[]);
+
+    async function searchContacts(){
+        try{
+            await api.get(`/contact/list/${pkMember}`).then(response => setContacts(response.data));
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+
+    const handleCheckboxChange = (contact: Contact) => {
+        setSelectedContacts((prevSelected) => {
+            const isSelected = prevSelected.some((c) => c.pk === contact.pk);
+            if (isSelected) {
+                return prevSelected.filter((c) => c.pk !== contact.pk);
+            } else {
+                return [...prevSelected, contact];
+            }
+        });
+    };
+
+    const isSelected = (contact: Contact) => {
+        return selectedContacts.some((c) => c.pk === contact.pk);
+    };
+
+    const handleRemove = (contact: Contact) => {
+        setSelectedContacts((prevSelected) =>
+            prevSelected.filter((c) => c.pk !== contact.pk)
+        );
+    };
+
+    async function createGroupChat(){
+        if(!titleGroup || selectedContacts.length === 0) {
+            return;
+        }
+
+        try{
+            const result = await api.post('/conversation/create', {
+                type_conversation: 1, 
+                title: titleGroup, 
+                list_pk_member: [
+                    pkMember,
+                    ...selectedContacts.map(contact => contact.pk)
+                ]
+            });
+
+            createGroup(result.data.data.pk_conversation);
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div className="group-modal-area">
+            <div className="group-modal-header">
+                <div>
+                    <span>Title:</span> 
+
+                    <input 
+                        type="text"
+                        name="title_conversation"
+                        value={titleGroup}
+                        onChange={ event => setTitleGroup(event.target.value) }
+                    />
+                </div>
+                <div className="group-modal-header-selected-contacts">
+                    {selectedContacts.map((contact) => (
+                        <span key={contact.pk} style={{ marginRight: '10px' }}>
+                            {contact.first_name}
+                            <button onClick={() => handleRemove(contact)}> X </button>
+                        </span>
+                    ))}
+                </div>
+                <button onClick={createGroupChat}>CREATE GROUP</button>
+            </div>
+            {contacts.map(contact => {
+                return (
+                    <div className="group-modal-item" key={contact.pk}>
+                        <span>{contact.first_name}</span>
+                        <input 
+                            type="checkbox"
+                            name="add_in_conversation"
+                            checked={isSelected(contact)}
+                            onChange={() => handleCheckboxChange(contact)}
+                        />
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -13,6 +13,7 @@ import { ContactsModal } from '../../components/ContactsModal/index';
 import { NewChatModal } from '../../components/NewChatModal/index';
 import Conversation from '../../components/Conversation';
 import MessageSession from '../../components/MessageSession';
+import { GroupModal } from '../../components/GroupModal';
 
 function Home() {
     const [isContactsModalOpen, setIsContactsModalOpen] = useState(false);
@@ -51,6 +52,11 @@ function Home() {
         closeChatModal();
         setPkConversationIsShow(pk_conversation);
     };
+    
+    const handleCreateGroup = (pk_conversation: string) => {
+        closeAddGroupModal();
+        setPkConversationIsShow(pk_conversation);
+    };
 
     return (
         <div className="home-content">
@@ -76,7 +82,10 @@ function Home() {
                                 closeModal={closeAddGroupModal}
                                 title='CREATE A GROUP'
                             >
-                                oi
+                                <GroupModal
+                                    pkMember={pkMember}
+                                    createGroup={handleCreateGroup}
+                                />
                             </Modal>
                         )}
 
