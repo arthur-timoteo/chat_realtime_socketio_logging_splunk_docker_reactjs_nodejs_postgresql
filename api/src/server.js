a6607c48f820707f2b9b6340c13228b2f9a7bd04
arthur <arthur.timoteo@gmail.com>
2025-06-12 19:30:01 -0300

    feat: [API] Improve start message

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,7 @@ const setupSocketIO = require('./service/socket.io');
 
 const PORT = process.env.PORT || 3001;
 
-const expressServer = app.listen(PORT, () => console.log("Server is running!"));
+const expressServer = app.listen(PORT, () => console.log(`API is running in the port ${PORT}!`));
 
 // Start the Socket.IO server
 setupSocketIO(expressServer);
